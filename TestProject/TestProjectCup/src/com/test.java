85c316192a6fb88b0c3332281cac4c419c692453
admin <admin@Lenovo-PC>
2018-10-30 14:32:18 +0530

    good luck

---
+++ TestProject/TestProjectCup/src/com/test.java
@@ -0,0 +1,17 @@
+package com;
+
+public class test {
+	
+	
+m1(){
+	
+	System.out.println("Swapnil");
+	
+}
+
+public static void main(String[] args) {
+	System.out.println("");
+	System.out.println("sandeep");
+}
+
+}
